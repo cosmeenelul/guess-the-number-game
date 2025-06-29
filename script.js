a91d6126755412577fbb05f4fee4fcdb2661f167
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-06-29 11:59:38 +0300

    add failure logic to the game

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20);
 document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const nrGhicit = Number(document.querySelector('.guess').value);
@@ -20,7 +21,23 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!nrGhicit) {
     document.querySelector('.message').textContent = `No number!❌`;
-  } else if (nrGhicit == secretNumber) {
+  } else if (nrGhicit === secretNumber) {
     document.querySelector('.message').textContent = `🎉 Correct number!`;
+  } else if (nrGhicit > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📈 Too High`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `🥲 You lost the game!`;
+    }
+  } else if (nrGhicit < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📉 Too Low`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `🥲 You lost the game!`;
+    }
   }
 });
