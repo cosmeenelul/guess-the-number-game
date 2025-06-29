e947140025ab3c54ac36bde2c5e9458516fe271e
cosmeenelul <cosmi.gabriel212004@gmail.com>
2025-06-29 13:36:27 +0300

    add again button functionality

--- script.js
+++ script.js
@@ -11,9 +11,8 @@
 
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-
 document.querySelector('.check').addEventListener('click', function () {
   const nrGhicit = Number(document.querySelector('.guess').value);
   console.log(nrGhicit, typeof nrGhicit);
@@ -24,6 +23,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // Correct guess
   } else if (nrGhicit === secretNumber) {
     document.querySelector('.message').textContent = `🎉 Correct number!`;
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
@@ -47,4 +47,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = 'red';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector('.guess').textContent = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
